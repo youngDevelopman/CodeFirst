4a97894eaf14c1ebb04457fceae7cb272c952297
Назар Мазурик <nazarmazurik99@gmail.com>
2018-05-22 00:06:46 +0300

    Add project files.

---
+++ CodeFirst/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (UserContext db = new UserContext())
+            {
+                var users = db.Users;
+                foreach (User u in users)
+                {
+                    Console.WriteLine("{0}.{1} - {2}", u.Id, u.Name, u.Age);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CodeFirst/UserContext.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Data.Entity;
+
+namespace CodeFirst
+{
+    class UserContext : DbContext
+    {
+        public UserContext() :
+            base("UserDB")
+        { }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<User> UsersTest { get; set; }
+    }
+}
